a839e4f9c4099e4e4dfdead937a12f141c0b243f
himanshubora7 <himanshubora972@gmail.com>
2025-09-28 13:00:47 +0530

    applet~ discontinued in newer version

---
+++ applet/p1.java
@@ -0,0 +1,12 @@
+package applet;
+
+import java.awt.*;
+import java.applet.*;
+
+public class p1 extends Applet {
+    public void paint(Graphics g) {
+        g.drawString("A simple applet trai ", 20, 20);
+    }
+}
+
+// APPLETs are discontinued from jaVA 9
